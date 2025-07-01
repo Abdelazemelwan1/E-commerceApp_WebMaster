ab6ef5a5d002e19146fc0828ab79f444f01a550d
Abdelazemelwan1 <abdelazemelwan123@gmail.com>
2025-07-01 18:40:18 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,22 @@ import {createHashRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 // import Layout from './components/Layout/Layout';
 // import Home from './components/Home/Home';
-import Shop from './components/Shop/Shop';
-import MeatsSeafood from './components/MeatsSeafood/MeatsSeafood';
-import Bakery from './components/Bakery/Bakery';
-import Beverages from './components/Beverages/Beverages';
-import Blog from './components/Blog/Blog';
-import Contact from './components/Contact/Contact';
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
-import NotFoundPage from './components/NotFoundPage/NotFoundPage';
-import HomeOne from './components/HomeOne/HomeOne';
-import HomeTwo from './components/HomeTwo/HomeTwo';
-import HomeThree from './components/HomeThree/HomeThree';
-import HomeFor from './components/HomeFor/HomeFor';
-import AboutUs from './components/AboutUs/AboutUs';
-import Layout from './components/Layout/Layout';
-import Home from './components/Home/Home';
+import Shop from './Components/Shop/Shop';
+import MeatsSeafood from './Components/MeatsSeafood/MeatsSeafood';
+import Bakery from './Components/Bakery/Bakery';
+import Beverages from './Components/Beverages/Beverages';
+import Blog from './Components/Blog/Blog';
+import Contact from './Components/Contact/Contact';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
+import NotFoundPage from './Components/NotFoundPage/NotFoundPage';
+import HomeOne from './Components/HomeOne/HomeOne';
+import HomeTwo from './Components/HomeTwo/HomeTwo';
+import HomeThree from './Components/HomeThree/HomeThree';
+import HomeFor from './Components/HomeFor/HomeFor';
+import AboutUs from './Components/AboutUs/AboutUs';
+import Layout from './Components/Layout/Layout';
+import Home from './Components/Home/Home';
 
 function App() {
   // const [count, setCount] = useState(0)
